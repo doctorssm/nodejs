bab62003a8af8ab63d030e0966f5ea1bad90f48a
Sergii Bogush <s.bogush1@gmail.com>
2019-02-10 22:11:26 +0200

    add products

---
+++ src/data/products.js
@@ -0,0 +1,27 @@
+const products = [{
+    productId: new Date().getTime(),
+    name: 'Product 1',
+    price: 199,
+    producer: 'China'
+  },
+  {
+    productId: new Date().getTime(),
+    name: 'Product 2',
+    price: 299,
+    producer: 'China'
+  },
+  {
+    productId: new Date().getTime(),
+    name: 'Product 3',
+    price: 399,
+    producer: 'UK'
+  },
+  {
+    productId: new Date().getTime(),
+    name: 'Product 4',
+    price: 499,
+    producer: 'Ukraine'
+  }
+];
+
+module.exports = products;
