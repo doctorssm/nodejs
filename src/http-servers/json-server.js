2104c5621cec09709055f27a6622e0481123d7a8
Sergii Bogush <s.bogush1@gmail.com>
2019-02-10 17:18:31 +0200

    add json server

---
+++ src/http-servers/json-server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const product = {
+    id: 1,
+    name: 'Supreme T-Shirt',
+    brand: 'Supreme',
+    price: 99.99,
+    options: [{
+            color: 'blue'
+        },
+        {
+            size: 'XL'
+        }
+    ]
+};
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+
+    res.write(JSON.stringify(product));
+    res.end();
+}).listen(5003);
