76445dfc3dca8b3a93e7d8781c77c3044e1e2cc7
Sergii Bogush <s.bogush1@gmail.com>
2019-02-10 21:05:58 +0200

    add routes

---
+++ src/routes/products.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/products', (req, res) => {
+    res.json({ productId: 10 });
+});
+
+router.get('/api/products/:id', (req, res) => {
+    res.json();
+});
+
+router.get('/api/products/:id/reviews', (req, res) => {
+    res.json();
+});
+
+router.post('/api/products', (req, res) => {
+    res.json();
+});
+
+module.exports = router;
---
+++ src/routes/user.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/users', (req, res) => {
+    res.json();
+});
+
+module.exports = router;
