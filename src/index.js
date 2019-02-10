c6ae2f9fcc8cdcbc0e0b5209b4f03ebc080cc134
Sergii Bogush <s.bogush1@gmail.com>
2019-02-10 20:39:26 +0200

    add cookie parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import app from './app';
-
+// import app from './app';
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    console.log('USE COOKIE', req.cookies);
+    req['parsedCookies'] = req.cookies;
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('USE QUERY', req.cookies);
+    req['parsedQuery'] = req.query;
+    next();
+});
+
+app.post('/', (req, res) => {
+    console.log('POST: parsedCookies', req.parsedCookies);
+    console.log('POST: parsedQuery', req.parsedQuery);
+    res.send();
+});
