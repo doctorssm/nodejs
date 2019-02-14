51d0263235833c30c8c9818475791f251d4700b1
Sergii Bogush <s.bogush1@gmail.com>
2019-02-14 21:56:31 +0200

    add product controller

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,45 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const products = require('./../data/products');
+const productController = require('./../controllers/product.controller');
 
-router.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-router.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product.productId === parseInt(req.params.id));
-
-    if (!product) {
-        // res.statusCode = 404;
-        throw 'Product not found';
-    }
-
-    res.json(product);
-});
-
-router.get('/api/products/:id/reviews', (req, res) => {
-    const product = products.find((product) => product.productId === parseInt(req.params.id));
-
-    if (!product) {
-        // res.statusCode = 404;
-        throw 'Product not found';
-    }
-
-    res.json(product.reviews);
-});
-
-router.post('/api/products', (req, res) => {
-    const product = {
-        productId: new Date().getTime(),
-        name: req.body.name,
-        price: req.body.price,
-        producer: req.body.producer,
-        reviews: req.body.reviews || 0
-    };
-
-    products.push(req.body);
-
-    res.json(product);
-});
+router.get('/api/products', productController.getProducts);
+router.get('/api/products/:id', productController.getProductById);
+router.get('/api/products/:id/reviews', productController.getProductReviews);
+router.post('/api/products', productController.addProduct);
 
 module.exports = router;
