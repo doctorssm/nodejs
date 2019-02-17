17926afdc51acfed72363e173408c4843bc69b0d
Sergii Bogush <s.bogush1@gmail.com>
2019-02-17 20:09:05 +0200

    add auth route

--- src/data/users.js
+++ src/data/users.js
@@ -1,18 +1,24 @@
 const users = [
   {
     userId: 1,
+    userName: 'dev',
     firstName: "John",
-    lastname: "Doe"
+    lastname: "Doe",
+    password: '123'
   },
   {
     userId: 2,
+    userName: 'test',
     firstName: "Bob",
-    lastname: "Clark"
+    lastname: "Clark",
+    password: '456'
   },
   {
     userId: 3,
+    userName: 'admin',
     firstName: "Amelie",
-    lastname: "Salo"
+    lastname: "Salo",
+    password: '789'
   }
 ];
 
