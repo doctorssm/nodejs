789e28c358a5c894e309d51a862ed03913fe0aca
Sergii Bogush <sergii_bogush@epam.com>
2019-02-18 16:24:41 +0200

    update to passport

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,32 @@ const express = require('express');
 const router = express.Router();
 const authController = require('./../controllers/auth.controller');
 const passport = require('./../config/passport');
+const jwt = require('jsonwebtoken');
+const config = require('./../config/config.json');
 
 // router.post('/api/auth', passport.authenticate('local'), authController.login);
 router.post("/api/auth", passport.authenticate('local', { session: false }), (req, res, next) => {
-res.json(req.user);
+// res.json(req.user);
+const token = jwt.sign({
+  sub: req.user.userId,
+  userName: req.user.userName
+}, config.secret, {
+  expiresIn: '2 hours'
+});
+
+res
+  .json({
+    code: 200,
+    message: 'OK',
+    data: {
+      user: {
+        email: req.user.email,
+        username: req.user.userName
+      }
+    },
+    token
+  });
+
 });
 
 module.exports = router;
