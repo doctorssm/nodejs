ffe6756a14ab5afff630f1d4364887fe1f3b51b3
Sergey Bogush <s.bogush1@gmail.com>
2019-04-14 12:04:48 +0300

    Hometask 8 (#7)
    * add swagger.yaml
    
    * crete swagger project
    
    * update path
    
    * ref
    
    * ref
    
    * add controllers

---
+++ swagger/api/controllers/customer.controller.js
@@ -0,0 +1,54 @@
+const Customer = require('../../../backend/models/customer.model');
+const helper = require('../../../backend/helpers/helper');
+
+module.exports.get = (req, res) => {
+  Customer.find((err, customers) => {
+    if (err) {
+      throw err;
+    }
+
+    res.json(customers);
+  });
+};
+
+module.exports.create = (req, res) => {
+  const customer = new Customer({
+    customerId: new Date().getTime(), // temp solution
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    comment: req.body.comment
+  });
+
+  Customer.create(customer, (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    res.json(result);
+  });
+};
+
+module.exports.updateOne = (req, res) => {
+  Customer.updateOne({ customerId: req.params.id }, req.body, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    res.json({
+      message: `Customer ${req.params.id} has been successfully updated`
+    });
+  });
+};
+
+module.exports.deleteMany = (req, res) => {
+  Customer.deleteMany({ customerId: { $in: helper.mapToNumbers(req.params.ids) } }, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    res.json({
+      message: `Customers ${req.params.ids} have been successfully deleted`
+    });
+  });
+};
